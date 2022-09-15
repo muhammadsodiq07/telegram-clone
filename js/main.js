b18503f35ee41278e38873a1ac318ab3729e6700
muhammadsodiq07 <muhammadodiqkoziyev1@gmail.com>
2022-09-15 21:22:44 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const infoPage = document.querySelector('.info-page');
+const stickerOpener = document.querySelector(".sticker-opener");
+const StickerPage = document.querySelector('.sticker-page');
+
+stickerOpener.addEventListener('click', ()=> {
+  infoPage.classList.toggle('sr-only')
+})
+
+
+
+
+
+// var name = prompt('your name');
+// var lastName = prompt('your last name')
+// var age = prompt('you age');
+// var passport = prompt('passport id');
+// var dollar = prompt('amount of money')
+// var dollarKurs = 10641;
+// var total = dollar * dollarKurs;
+
+// console.log('name: ' +  name  + ',\nlastname ' +  lastName  + ',\nage: ' +  age  + ',\npassport id: ' +  passport  + ',\n dollar: ' +  dollar + total)
+
+
+
+
+
